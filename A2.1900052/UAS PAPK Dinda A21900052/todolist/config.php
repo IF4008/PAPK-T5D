bc4a50dbd60b0407c558e5c59ebced87d22e6de6
dindatr <a2.1900052@mhs.stmik-sumedang.ac.id>
2022-01-16 23:11:38 +0700

    UAS

---
+++ A2.1900052/UAS PAPK Dinda A21900052/todolist/config.php
@@ -0,0 +1,25 @@
+<?php
+error_reporting(0);
+session_start();
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'todolist');
+define("SITE_KEY", 'MariBelajarcoding');
+function getDB() 
+{ $dbhost=DB_SERVER;
+ $dbuser=DB_USERNAME;
+ $dbpass=DB_PASSWORD;
+ $dbname=DB_DATABASE;
+ $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass); 
+ $dbConnection->exec("set names utf8");
+ $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ return $dbConnection;
+}
+function apiToken($session_uid)
+{
+$key=md5(SITE_KEY.$session_uid);
+return hash('sha256', $key);
+}
+?>
